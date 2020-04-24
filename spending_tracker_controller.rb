7ac2014edbbdfb7c0eea757799617b8628612180
Valerian Roustit <valerian.roustit@gmail.com>
2020-04-24 16:12:10 +0100

    first commit

---
+++ spending_tracker_controller.rb
@@ -0,0 +1,21 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('models/')
+require_relative('models/')
+also_reload('./models/*')
+require('pry')
+
+
+# index
+
+# new
+
+# show
+
+# create
+
+# edit
+
+# update
+
+# delete
