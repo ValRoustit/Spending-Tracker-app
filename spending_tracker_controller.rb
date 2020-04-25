b7a4191156b08bd1aae05d9b4a6baf13404f45e9
Valerian Roustit <valerian.roustit@gmail.com>
2020-04-25 14:08:13 +0100

    add in views index, layout, new_spending, show_merchants, show_tags, show_transactions

--- spending_tracker_controller.rb
+++ spending_tracker_controller.rb
@@ -12,13 +12,31 @@ get '/spending-tracker' do
     erb(:index)
 end
 
-# new tag
+# show_transactions
+get '/spending-tracker/my-spendings' do
+    @transactions = Transaction.all
+    erb(:show_transactions)
+end
+
+# show_tags
+get '/spending-tracker/my-tags' do
+    @tags = Tag.all
+    erb(:show_tags)
+end
+
+# show_merchants
+get '/spending-tracker/my-merchants' do
+    @merchants = Merchant.all
+    erb(:show_merchants)
+end
 
 # new merchant
 
 # new transaction
+get '/spending-tracker/new' do
+    erb(:new_spending)
+end
 
-# show
 
 # create
 
