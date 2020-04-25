40907a0a270f68fcb78374095fe5d6c0af1be591
Valerian Roustit <valerian.roustit@gmail.com>
2020-04-25 10:35:32 +0100

    add update, find and all in Transaction

--- models/transaction.rb
+++ models/transaction.rb
@@ -19,6 +19,29 @@ class Transaction
         @id = transaction['id'].to_i
     end
 
+    def self.find(id)
+        sql = "SELECT * FROM transactions WHERE id = $1"
+        values = [id]
+        result = SqlRunner.run(sql, values).first
+        transaction = Transaction.new(result)
+        return transaction
+    end
+
+    def update()
+        sql = "UPDATE transactions SET 
+        (name, amount, merchant_id, tag_id)
+         = 
+        ($1, $2, $3, $4) WHERE id = $5"
+        values = [@name, @amount, @merchant_id, @tag_id, @id]
+        SqlRunner.run(sql, values)
+    end
+
+
+    def self.all()
+        sql = "SELECT * FROM transactions"
+        transaction_data = SqlRunner.run(sql)
+        return Transaction.map_items(transaction_data)
+    end
 
     def delete()
         sql = "DELETE FROM transactions WHERE id = $1"
