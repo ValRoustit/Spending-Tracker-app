a3f2c089e0d77398dc05e379238a74b3000f6d81
Valerian Roustit <valerian.roustit@gmail.com>
2020-04-25 11:17:57 +0100

    add index in controller

--- spending_tracker_controller.rb
+++ spending_tracker_controller.rb
@@ -1,14 +1,22 @@
 require('sinatra')
 require('sinatra/contrib/all')
-require_relative('models/')
-require_relative('models/')
+require_relative('models/merchant')
+require_relative('models/tag')
+require_relative('models/transaction')
 also_reload('./models/*')
 require('pry')
 
 
 # index
+get '/spending-tracker' do 
+    erb(:index)
+end
 
-# new
+# new tag
+
+# new merchant
+
+# new transaction
 
 # show
 
